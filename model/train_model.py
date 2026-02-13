1f1164fb65c9ed4cdb00e37487811415ef340a77
chitranjan <kchitranjan7@gmail.com>
2026-02-13 21:59:19 +0530

    Uploaded trained model

---
+++ model/train_model.py
@@ -0,0 +1,23 @@
+from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+import joblib
+
+# Load dataset
+data = load_breast_cancer()
+X = data.data
+y = data.target
+
+# Split data
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+# Train SVM model
+model = SVC(probability=True)
+model.fit(X_train, y_train)
+
+# Save model (THIS creates svm_model.pkl automatically)
+joblib.dump(model, "svm_model.pkl")
+
+print("✅ Model saved successfully")
